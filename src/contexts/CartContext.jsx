4bd07b0008e7177ced2d2aa81846566fa6c66255
Abdullah2232006 <abdullah2232006@gmail.com>
2026-08-20 14:44:08 +0300

    Add CartContext basic react

---
+++ src/contexts/CartContext.jsx
@@ -0,0 +1,11 @@
+import { createContext, useContext, useState } from "react";
+
+export const CartContext = createContext();
+
+export function CartProvider({ children }) {
+    return(
+        <CartContext.Provider>
+            {children}
+        </CartContext.Provider>
+    )
+}
